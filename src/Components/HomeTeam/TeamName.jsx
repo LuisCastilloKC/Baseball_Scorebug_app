dff1b9bab929f72a24d249df55015947365b16cb
Luis Castillo <mr.luiscastillo@gmail.com>
2022-01-19 16:50:12 -0500

    create TeamName for HomeTeam

---
+++ src/Components/HomeTeam/TeamName.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TeamName = () => {
+
+    return(
+        <div> 
+            <h4>Team Name</h4>
+            <button>-</button>
+            <button>+</button>
+        </div>
+    )
+};
+
+export default TeamName;
