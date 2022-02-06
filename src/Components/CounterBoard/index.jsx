3fb819f94c0924240477468f09fd4b2299567c7f
Luis Castillo <mr.luiscastillo@gmail.com>
2022-02-05 23:58:04 -0500

    display button to counter

--- src/Components/CounterBoard/index.jsx
+++ src/Components/CounterBoard/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import CounterButton from './CounterButton';
 
 const CounterBoard = () =>{
 
     return(
+        <> 
         <div>
             <div> 0</div>
             <div> 0 - 0</div>
             <div>0 Outs</div>
         </div>
+        <CounterButton />
+        </>
     )
 }
 
